51f1d8d46bccf0db936097516abeaa29ead2fa53
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-10-16 15:36:40 +0100

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+---
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = nextConfig
+
+---
+postcss.config.js
+---
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+---
